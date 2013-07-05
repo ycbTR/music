dfadd11759b8c74cfcd831e3162009942f51db1a
nasirvq <itsnasir@gmail.com>
2013-07-05 19:04:51 +0500

    new layout

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'soundcloud'
 
 gem 'xml-simple'
 
+gem 'therubyracer'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,118 +4,119 @@ require "xmlsimple"
 
 class UsersController < ApplicationController
 
- # layout "back_layout", :only=>[:index]
+  # layout "back_layout", :only=>[:index]
   layout "front_layout", :only=>[:home]
 
-  def home 
-	
+  def home
+
   end
 
   def index
-       if params[:search_fild].nil?
-	redirect_to root_path
-       else
-        session[:keyword] = params[:search_fild]
-       end
+    if params[:search_fild].nil?
+      redirect_to root_path
+    else
+      session[:keyword] = params[:search_fild]
+    end
   end
 
   def search
 
-	keyword = params[:keyword].split.join('%20')
-	begin
+    keyword = params[:keyword].split.join('%20')
+    type = params[:type]
+    begin
 
-	data = open("http://tinysong.com/s/#{keyword}?format=json&limit=100&key=5d5a73becf2fbb5f9c59f801b9edeb6c").read
+      data = open("http://tinysong.com/s/#{keyword}?format=json&limit=100&key=5d5a73becf2fbb5f9c59f801b9edeb6c").read
 
-	puts "DATA #{data}"
-	@playlist = JSON.parse(data)
-        puts "PARSED DATA #{@playlist}"
-	rescue Exception=>e
-		puts "EXCEPTION :: #{e.message}"
-	end
-     respond_to do |format|
+      puts "DATA #{data}"
+      @playlist = JSON.parse(data)
+      puts "PARSED DATA #{@playlist}"
+    rescue Exception=>e
+      puts "EXCEPTION :: #{e.message}"
+    end
+    respond_to do |format|
       format.js
-     end
+    end
 
   end
 
   def search_spotify
-	keyword = params[:keyword].split.join('+')
-	begin
+    keyword = params[:keyword].split.join('+')
+    begin
 
-	data = open("http://ws.spotify.com/search/1/track.json?q=#{keyword}").read
+      data = open("http://ws.spotify.com/search/1/track.json?q=#{keyword}").read
 
-	puts "DATA #{data}"
-	@playlist = JSON.parse(data)['tracks']
-        puts "PARSED DATA #{@playlist}"
-	rescue Exception=>e
-		puts "EXCEPTION :: #{e.message}"
-	end
-     respond_to do |format|
+      puts "DATA #{data}"
+      @playlist = JSON.parse(data)['tracks']
+      puts "PARSED DATA #{@playlist}"
+    rescue Exception=>e
+      puts "EXCEPTION :: #{e.message}"
+    end
+    respond_to do |format|
       format.js
-     end
+    end
   end
 
 
   def search_soundcloud
 
-	keyword = params[:keyword]
-	begin
-	
-	# create a client object with your app credentials
-	client = Soundcloud.new(:client_id => 'f7e97d20f4e0a7044abfb7a4e79700f0')
+    keyword = params[:keyword]
+    begin
 
-	# find all sounds of buskers licensed under 'creative commons share alike'
-	tracks = client.get('/tracks', :q => keyword,:limit => 100, :licence => 'cc-by-sa')
+      # create a client object with your app credentials
+      client = Soundcloud.new(:client_id => 'f7e97d20f4e0a7044abfb7a4e79700f0')
 
-	puts "TRACKS #{tracks}"
-	@playlist = tracks #JSON.parse(tracks)
-        puts "PARSED DATA #{@playlist}"
-	rescue Exception=>e
-		puts "EXCEPTION :: #{e.message}"
-	end
-     respond_to do |format|
+      # find all sounds of buskers licensed under 'creative commons share alike'
+      tracks = client.get('/tracks', :q => keyword,:limit => 100, :licence => 'cc-by-sa')
+
+      puts "TRACKS #{tracks}"
+      @playlist = tracks #JSON.parse(tracks)
+      puts "PARSED DATA #{@playlist}"
+    rescue Exception=>e
+      puts "EXCEPTION :: #{e.message}"
+    end
+    respond_to do |format|
       format.js
-     end
+    end
   end
 
 
   def search_rdio
 
-	keyword = params[:keyword]
-	begin
-	puts "AAAAAAAAAAAAA"
-	rdio = Rdio.new(["cvswsfupv9d2svsfm936mxjs", "fG5t3EF7eG"])
-	puts "KKKKKKKKKKKKKKK"
-	data =  rdio.call('search', {'query'=>keyword,'types'=>'Artist,Album,Track,Playlist','count'=>100})
-	puts "BBBBBBBBBBBB"
-	@playlist = data['result']['results']
-	puts "CCCCCCCCCCCCCCCCC #{@playlist}"
-        puts "PARSED DATA #{@playlist}"
-	rescue Exception=>e
-		puts "EXCEPTION :: #{e.message}"
-	end
-     respond_to do |format|
+    keyword = params[:keyword]
+    begin
+      puts "AAAAAAAAAAAAA"
+      rdio = Rdio.new(["cvswsfupv9d2svsfm936mxjs", "fG5t3EF7eG"])
+      puts "KKKKKKKKKKKKKKK"
+      data =  rdio.call('search', {'query'=>keyword,'types'=>'Artist,Album,Track,Playlist','count'=>100})
+      puts "BBBBBBBBBBBB"
+      @playlist = data['result']['results']
+      puts "CCCCCCCCCCCCCCCCC #{@playlist}"
+      puts "PARSED DATA #{@playlist}"
+    rescue Exception=>e
+      puts "EXCEPTION :: #{e.message}"
+    end
+    respond_to do |format|
       format.js
-     end
+    end
   end
 
 
   def search_youtube
 
-	keyword = params[:keyword].split.join('%2C')
-	#uri = "http://gdata.youtube.com/feeds/api/videos/-/music%7Csongs/#{keyword}?v=2&start-index=1&max-results=100"
-	uri = "http://gdata.youtube.com/feeds/api/videos?category=#{keyword}%2Cmusic%7Csongs&v=2"
-	puts "URI #{uri}"
-	begin
-	entries=open(uri).read
-	puts "entries #{entries}"
-	@playlist = XmlSimple.xml_in(entries)["entry"]
-	rescue Exception=>e
-		puts "EXCEPTION :: #{e.message}"
-	end
-     respond_to do |format|
+    keyword = params[:keyword].split.join('%2C')
+    #uri = "http://gdata.youtube.com/feeds/api/videos/-/music%7Csongs/#{keyword}?v=2&start-index=1&max-results=100"
+    uri = "http://gdata.youtube.com/feeds/api/videos?category=#{keyword}%2Cmusic%7Csongs&v=2"
+    puts "URI #{uri}"
+    begin
+      entries=open(uri).read
+      puts "entries #{entries}"
+      @playlist = XmlSimple.xml_in(entries)["entry"]
+    rescue Exception=>e
+      puts "EXCEPTION :: #{e.message}"
+    end
+    respond_to do |format|
       format.js
-     end
+    end
 
   end
 
